ae5689d9f6831e40841cbdd144ca3ad460f4a7b5
AG60758 <AG60758@L4DCXZY2.us.ad.wellpoint.com>
2020-04-25 18:04:02 +0530

    All DAOs Implmented

---
+++ dao-impl/src/main/java/com/tracker/dao_impl/PatientDAOImpl.java
@@ -0,0 +1,37 @@
+package com.tracker.dao_impl;
+
+import java.util.List;
+
+import com.tracker.server.dao.PatientDAO;
+import com.tracker.server.entities.Country;
+import com.tracker.server.entities.Patient;
+import com.tracker.server.entities.State;
+
+public class PatientDAOImpl implements PatientDAO {
+
+	public Patient getPatientDetails(Patient patient) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Patient> getAllPatients() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Patient> gtPatientsByRegion(Patient patient) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Patient> getPatientsByState(State state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Patient> getPatientsByCountry(Country country) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ dao-impl/src/main/java/com/tracker/dao_impl/RegionDAOImpl.java
@@ -0,0 +1,20 @@
+package com.tracker.dao_impl;
+
+import java.util.List;
+
+import com.tracker.server.dao.RegionDAO;
+import com.tracker.server.entities.Region;
+
+public class RegionDAOImpl implements RegionDAO {
+
+	public Region getRegion(Region region) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Region> getAllRegions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
