1e588c449b21e2c678760ceac72e22b82526a07d
AG60758 <AG60758@L4DCXZY2.us.ad.wellpoint.com>
2020-04-28 11:13:35 +0530

    Customer entity added.

---
+++ simplebootapp-inv/src/main/java/com/example/simplebootapp/entities/Customer.java
@@ -0,0 +1,55 @@
+package com.example.simplebootapp.entities;
+
+import java.sql.Date;
+
+public class Customer {
+
+	
+	
+	private Long customerId;
+	private String customerName;
+	private Date dob;
+	private Long phoneNo;
+	private String address;
+	private String email;
+	
+	
+	
+	public Long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public Long getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(Long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
